6556e23170f74948780cfb845d26b0797226e06e
razvshakya <45102400+razvshakya@users.noreply.github.com>
2020-12-03 23:21:24 -0500

    Add files via upload
    Testing upload from local drive

---
+++ trtle.py
@@ -0,0 +1,10 @@
+from turtle import left, backward, right, penup, pendown
+
+def square():
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
